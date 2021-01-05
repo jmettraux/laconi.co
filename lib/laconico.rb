bea40a5ae751b4f8c7a024376d029789551062fc
John Mettraux <jmettraux@gmail.com>
2021-01-05 17:17:20 +0900

    Fix laconico.rb vs log and tail and itself

--- lib/laconico.rb
+++ lib/laconico.rb
@@ -5,8 +5,9 @@ require 'redcarpet'
 Dir[File.join(__dir__, '*.rb')]
   .each do |pa|
 
-    require(pa) \
-      unless %w[ laconico.rb tail.rb log.rb ].find { |e| pa.end_with?(pa) }
+    next if %w[ laconico.rb tail.rb log.rb ].find { |e| pa.end_with?(e) }
+
+    require(pa)
   end
 
 def neutralize_name(s); s.gsub(/[^a-zA-Z]/, '_'); end
